7b781e04f8fa2fb41feccd76825d163e023a94bb
Shishir Talukder <shishir.talukdar.017@gmail.com>
2026-07-14 18:19:55 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-  const base = process.env.GITHUB_ACTIONS === "true" && repositoryName ? `/${repositoryName}/` : "/";
+const base = "/";
+
 
   return {
     base,
